33dc22687e6ab164158614616804aaac2b44342a
jo-im <joann_im@hotmail.com>
2016-07-08 20:13:01 -0700

    "Fully functional links/shortener"

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -2,15 +2,18 @@ angular.module('shortly.links', [])
 
 .controller('LinksController', function ($scope, Links) {
   $scope.usefulData = [];
+
   $scope.data = {};
   var first = Links.getAll().then(function (links) {
     $scope.data.links = links;
-    $scope.usefulData =$scope.data.links.map(function(x){return "shortly.com/"+x.code});
-  console.log($scope.usefulData)
+    $scope.usefulData =$scope.data.links.map(function(x){return ["shortly.com/"+x.code,x.title,x.url]});
+
+    console.log($scope.usefulData);
+
+
+    
+    })
   });
 
-
-
-});
 
 
